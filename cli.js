4b0074684fd79acb17fe95fb9b1926acc4b21a44
Erick Merchant <erickmerchant@gmail.com>
2017-07-18 21:39:15 -0700

    Rewrite render

--- cli.js
+++ cli.js
@@ -1,71 +1,80 @@
 #!/usr/bin/env node
-const fs = require('fs')
-const path = require('path')
-const mkdirp = require('mkdirp')
-const assert = require('assert')
-const cheerio = require('cheerio')
 const command = require('sergeant')
-const error = require('sergeant/error')
-const chokidar = require('chokidar')
+const JSDOM = require('jsdom').JSDOM
+const path = require('path')
+const globParent = require('glob-parent')
+const thenify = require('thenify')
+const mkdirp = thenify(require('mkdirp'))
+const glob = thenify(require('glob'))
+const readFile = thenify(require('fs').readFile)
+const writeFile = thenify(require('fs').writeFile)
 
 command('framework', 'some helpful commands for your app', function ({parameter, option, command}) {
   command('render', 'render a component to static html', function ({parameter, option}) {
-    parameter('renderer', {
-      description: 'the module that does the rendering',
+    parameter('state', {
+      description: 'a glob to json files',
       required: true
     })
 
-    option('watch', {
-      description: 'watch for changes',
-      type: Boolean,
-      aliases: ['w'],
-      default: false
+    parameter('component', {
+      description: 'the component',
+      required: true
+    })
+
+    parameter('document', {
+      description: 'the target html document',
+      required: true
+    })
+
+    option('selector', {
+      description: 'a selector to find in the document',
+      default: 'body'
+    })
+
+    option('output', {
+      description: 'a directory to save to. Defaults to dirname of <document>',
+      aliases: ['o']
     })
 
     return function (args) {
-      run(args.renderer)
+      const component = require(path.join(process.cwd(), args.component))
+      let outputDirectory = args.output
 
-      if (args.watch) {
-        chokidar.watch([args.renderer], {ignoreInitial: true}).on('all', () => {
-          run(args.renderer)
-        })
+      if (!outputDirectory) {
+        outputDirectory = path.dirname(path.join(process.cwd(), args.document))
       }
+
+      return readFile(path.join(process.cwd(), args.document), 'utf8').then((html) => {
+        const stateParent = globParent(args.state)
+
+        return glob(args.state).then((files) => {
+          return Promise.all(files.map((file) => {
+            const dom = new JSDOM(html)
+            const state = require(path.join(process.cwd(), file))
+            const element = dom.window.document.querySelector(args.selector)
+
+            if (element) {
+              element.innerHTML = String(component({state, dispatch, next}))
+            }
+
+            const result = dom.serialize()
+            const relativeFile = path.relative(stateParent, file)
+            const outputFile = path.join(outputDirectory, path.dirname(relativeFile), path.basename(relativeFile, path.extname(relativeFile))) + '.html'
+
+            return mkdirp(path.dirname(outputFile)).then(() => {
+              return writeFile(outputFile, result)
+            })
+          }))
+        })
+      })
     }
   })
 })(process.argv.slice(2))
 
-function run (renderer) {
-  try {
-    delete require.cache[path.join(process.cwd(), renderer)]
-
-    const required = require(path.join(process.cwd(), renderer))
-
-    assert.equal(typeof required, 'function', 'the required module should be a function')
-
-    required(render)
-  } catch (e) {
-    error(e)
-
-    process.exit(1)
-  }
+function dispatch () {
+  throw new Error('dispatch is unavailable')
 }
 
-function render (inpath, selector, save) {
-  fs.readFile(inpath, 'utf-8', function (err, content) {
-    if (err) throw err
+function next () {
 
-    const $ = cheerio.load(content)
-
-    save(function (outpath, html) {
-      mkdirp(path.dirname(outpath), function (err) {
-        if (err) throw err
-
-        $(selector).replaceWith(String(html))
-
-        fs.writeFile(outpath, $.html(), function (err) {
-          if (err) throw err
-        })
-      })
-    })
-  })
 }
