36b5035629a176df2e85b589e162aa3f4a3d309b
Erick Merchant <erickmerchant@gmail.com>
2017-12-12 22:07:56 -0800

    Restructure

--- cli.js
+++ cli.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 const command = require('sergeant')
-const JSDOM = require('jsdom').JSDOM
-const path = require('path')
-const commondir = require('commondir')
-const thenify = require('thenify')
-const mkdirp = thenify(require('mkdirp'))
-const glob = thenify(require('glob'))
-const readFile = thenify(require('fs').readFile)
-const writeFile = thenify(require('fs').writeFile)
+const render = require('./index').render
 
 command('framework', 'some helpful commands for your app', function ({parameter, option, command}) {
   command('render', 'render a component to static html', function ({parameter, option}) {
@@ -38,50 +31,6 @@ command('framework', 'some helpful commands for your app', function ({parameter,
       aliases: ['o']
     })
 
-    return function (args) {
-      const component = require(path.join(process.cwd(), args.component))
-      let outputDirectory = args.output
-
-      if (!outputDirectory) {
-        outputDirectory = path.dirname(path.join(process.cwd(), args.document))
-      }
-
-      return readFile(path.join(process.cwd(), args.document), 'utf8').then((html) => {
-        return Promise.all(args.state.map((state) => glob(state, {nodir: true})))
-        .then((files) => {
-          files = files.reduce((files, current) => files.concat(current), [])
-
-          const stateParent = commondir(process.cwd(), files)
-
-          return Promise.all(files.map((file) => {
-            const dom = new JSDOM(html)
-            const state = require(path.join(process.cwd(), file))
-            const element = dom.window.document.querySelector(args.selector)
-
-            if (element) {
-              const fragment = new JSDOM(String(component({state, dispatch, next})))
-
-              element.parentNode.replaceChild(fragment.window.document.body, element)
-            }
-
-            const result = dom.serialize()
-            const relativeFile = path.relative(stateParent, file)
-            const outputFile = path.join(outputDirectory, path.dirname(relativeFile), path.basename(relativeFile, path.extname(relativeFile))) + '.html'
-
-            return mkdirp(path.dirname(outputFile)).then(() => {
-              return writeFile(outputFile, result)
-            })
-          }))
-        })
-      })
-    }
+    return render
   })
 })(process.argv.slice(2))
-
-function dispatch () {
-  throw new Error('dispatch is unavailable')
-}
-
-function next () {
-
-}
---
+++ index.js
@@ -0,0 +1,54 @@
+const JSDOM = require('jsdom').JSDOM
+const path = require('path')
+const commondir = require('commondir')
+const thenify = require('thenify')
+const mkdirp = thenify(require('mkdirp'))
+const glob = thenify(require('glob'))
+const readFile = thenify(require('fs').readFile)
+const writeFile = thenify(require('fs').writeFile)
+
+exports.render = function (args) {
+  const component = require(path.join(process.cwd(), args.component))
+  let outputDirectory = args.output
+
+  if (!outputDirectory) {
+    outputDirectory = path.dirname(path.join(process.cwd(), args.document))
+  }
+
+  return readFile(path.join(process.cwd(), args.document), 'utf8').then((html) => {
+    return Promise.all(args.state.map((state) => glob(state, {nodir: true})))
+    .then((files) => {
+      files = files.reduce((files, current) => files.concat(current), [])
+
+      const stateParent = commondir(process.cwd(), files)
+
+      return Promise.all(files.map((file) => {
+        const dom = new JSDOM(html)
+        const state = require(path.join(process.cwd(), file))
+        const element = dom.window.document.querySelector(args.selector)
+
+        if (element) {
+          const fragment = new JSDOM(String(component({state, dispatch, next})))
+
+          element.parentNode.replaceChild(fragment.window.document.body, element)
+        }
+
+        const result = dom.serialize()
+        const relativeFile = path.relative(stateParent, file)
+        const outputFile = path.join(outputDirectory, path.dirname(relativeFile), path.basename(relativeFile, path.extname(relativeFile))) + '.html'
+
+        return mkdirp(path.dirname(outputFile)).then(() => {
+          return writeFile(outputFile, result)
+        })
+      }))
+    })
+  })
+}
+
+function dispatch () {
+  throw new Error('dispatch is unavailable')
+}
+
+function next () {
+
+}
