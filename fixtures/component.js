d1668c3bb3d1ba4ba17c21f804d655f0f0a74fb6
Erick Merchant <erickmerchant@gmail.com>
2018-10-26 08:15:23 -0700

    Remove nanomorph as dev dependency

--- fixtures/component.js
+++ fixtures/component.js
@@ -1,5 +1,3 @@
-const html = require('nanohtml')
-
 module.exports = function ({ state }) {
-  return html`<main><h1>${state.heading}</h1></main>`
+  return `<main><h1>${state.heading}</h1></main>`
 }
